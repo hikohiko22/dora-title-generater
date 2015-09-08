2a82332e8f90ff20c46634bb9d1b213e0513ea3f
pepoipod <daikinecmouth@hotmail.co.jp>
2015-09-08 22:59:07 +0900

    draw image test

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
+var fs = require('fs');
 var express = require('express');
 var app = express();
 
 var Canvas = require('canvas');
-var canvas = new Canvas(200,200);
-var ctx = canvas.getContext('2d');
+//var canvas = new Canvas(200,200);
+//var ctx = canvas.getContext('2d');
 
-ctx.font = '30px Impact';
-ctx.rotate(.1);
-ctx.fillText("Hello World!", 50, 100);
+fs.readFile(__dirname + '/pronama.png', function(err, data){
+    if (err) throw err;
+
+    // データをcanvasのcontextに設定
+    var img = new Image;
+    img.src = data;
+    var canvas = new Canvas(img.width, img.height);
+    var ctx = canvas.getContext('2d');
+    ctx.drawImage(img, 0, 0, img.width, img.height);
+});
+
+//ctx.font = '30px Impact';
+//ctx.rotate(.1);
+//ctx.fillText("Hello World!", 50, 100);
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
