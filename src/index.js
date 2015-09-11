6cb3bd5d874a6e6ecb185492907af1243c0253b9
pepoipod <daikinecmouth@hotmail.co.jp>
2015-09-11 17:13:26 +0900

    set text align test

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ fs.readFile(__dirname + '/images/bg.jpg', function(err, data){
     ctx.textAlign = 'center';
 
 	ctx.fillStyle = 'rgba(255, 255, 255, 1.0)';
-	ctx.fillText(text, canvas.width / 2 - textMeasure.width / 2, 100);
+	ctx.fillText(text, canvas.width / 2, 100);
 
 	console.log(canvas.width, textMeasure.width);
 
