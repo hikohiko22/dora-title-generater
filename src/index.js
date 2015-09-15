383fca1d85f819ec360bd8ef9b6823c3166481ec
pepoipod <daikinecmouth@hotmail.co.jp>
2015-09-15 17:18:50 +0900

    request test

--- src/index.js
+++ src/index.js
@@ -10,46 +10,45 @@ var app = express();
 fs.readFile(__dirname + '/images/bg.jpg', function(err, data){
     if (err) throw err;
 
-    app.set('port', (process.env.PORT || 5000));
-	app.use(express.static(__dirname + '/public'));
-
-	app.get('/:txt', function(request, response) {
-		console.log('request.params.txt');
-	    console.log(request.params.txt);
-
-	    // データをcanvasのcontextに設定
+    // データをcanvasのcontextに設定
     var img = new Image;
     img.src = data;
     var canvas = new Canvas(img.width, img.height);
     var ctx = canvas.getContext('2d');
     ctx.drawImage(img, 0, 0, img.width, img.height);
 
-    var text = 'どこでもドア岩本はすごくたのしいんだ。すごいよ＝＝＝＝＝＝えへへへっへへｈ従来の方式では、全ての';
-    var textMeasure;
+    app.set('port', (process.env.PORT || 5000));
+	app.use(express.static(__dirname + '/public'));
 
-    ctx.font = '45px "Rounded Mgen+ 1c"';
-    ctx.textAlign = 'center';
-	ctx.fillStyle = 'rgba(255, 255, 255, 1.0)';
+	app.get('/:txt', function(request, response) {
 
-    var tmpText = '';
-    var textPosY = 105;
+		var text = request.params.txt;
+	    var textMeasure;
 
-    for (var i = 0, len = text.length; i < len; i++) {
-    	tmpText += text[i];
-    	textMeasure = ctx.measureText(tmpText);
+	    ctx.font = '45px "Rounded Mgen+ 1c"';
+	    ctx.textAlign = 'center';
+		ctx.fillStyle = 'rgba(255, 255, 255, 1.0)';
 
-    	if (textMeasure.width >= 370) {
-    		ctx.fillText(tmpText, canvas.width / 2, textPosY);
+	    var tmpText = '';
+	    var textPosY = 105;
 
-    		tmpText = '';
-    		textPosY += 50;
-    	}
-    }
+	    for (var i = 0, len = text.length; i < len; i++) {
+	    	tmpText += text[i];
+	    	textMeasure = ctx.measureText(tmpText);
 
-    if (tmpText.length !== 0) {
-    	ctx.fillText(tmpText, canvas.width / 2, textPosY);
-    	tmpText = '';
-    }
+	    	if (textMeasure.width >= 370) {
+	    		ctx.fillText(tmpText, canvas.width / 2, textPosY);
+
+	    		tmpText = '';
+	    		textPosY += 50;
+	    	}
+	    }
+
+	    if (tmpText.length !== 0) {
+	    	ctx.fillText(tmpText, canvas.width / 2, textPosY);
+	    	tmpText = '';
+	    }
+	    response.send('<img src="' + canvas.toDataURL() + '" />');
 	});
 
 	app.listen(app.get('port'), function() {
